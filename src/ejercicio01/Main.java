1be853d473123c4a5d804f90e16ccd34ae11c947
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2024-01-19 13:02:31 +0100

    Ejercicio 1 hecho

--- src/ejercicio01/Main.java
+++ src/ejercicio01/Main.java
@@ -2,8 +2,20 @@ package ejercicio01;
 
 public class Main {
 
+	/*
+	 * Crearemos un programa con una clase que contenga una función para sumar todo
+	 * lo que haya dentro de un Array para luego guardarlo en una variable, que
+	 * luego imprimiremos en pantalla.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Creamos el Array en cuestión para probar su funcionamiento
+		int[] tabla = { 20, 35, 34, 100, 404 };
+
+		// Invocamos la función y guardamos lo que retorne al result.
+		int result = TablaParametros.sumarTabla(tabla);
+
+		// Imprimimos lo retornado.
+		System.out.println(result);
 
 	}
 
--- src/ejercicio01/TablaParametros.java
+++ src/ejercicio01/TablaParametros.java
@@ -2,4 +2,21 @@ package ejercicio01;
 
 public class TablaParametros {
 
+	/*
+	 * Crearemos una clase que dado un array, devuelva la suma de todos sus
+	 * elementos.
+	 */
+	public static int sumarTabla(int[] tabla) {
+		// Creamos la variable result para devolverla
+		int result = 0;
+
+		// Hacemos un foreach que recorra todo el Array y los valores que encuentre los
+		// irá sumando a result.
+		for (int i : tabla) {
+			result += i;
+		}
+
+		// Lo retornamos.
+		return result;
+	}
 }
