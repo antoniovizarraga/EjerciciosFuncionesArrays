93ae02b4e6f28d972a4d8a8524e31c0b3d35ee92
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2024-01-19 14:23:09 +0100

    Ejercicio 4 terminado

--- src/ejercicio04/Main.java
+++ src/ejercicio04/Main.java
@@ -2,11 +2,21 @@ package ejercicio04;
 
 public class Main {
 
+	/* Programa que dado un array, buscará de forma
+	 * secuencial una clave dada en dicho Array.
+	 * Lo haremos en una función que le
+	 * pasaremos como parámetro un Array
+	 * y el valor a buscar. */
 	public static void main(String[] args) {
+		
+		// Array que contendrá los valores.
 		int[] tabla = {1, 6, 8, 404, 30, 25};
 		
+		
+		// Variable que contendrá la posición del elemento en el Array.
 		int result = TablaFunciones.buscar(tabla, 404);
 		
+		// Imprimimos los resultados.
 		System.out.println("Posición en el Array: " + result);
 
 	}
--- src/ejercicio04/TablaFunciones.java
+++ src/ejercicio04/TablaFunciones.java
@@ -2,18 +2,31 @@ package ejercicio04;
 
 public class TablaFunciones {
 
+	/*
+	 * Creamos la función con los parámetros de Array en el que buscaremos el valor,
+	 * y el valor en sí a buscar (Clave).
+	 */
 	public static int buscar(int t[], int clave) {
-		
-		int result = 0;
-		
-		for(int i = 0; i < t.length; i++) {
-			if(t[i] == clave) {
-				result = i;
-				break;
-			}
+
+		// La variable que retornaremos que contendrá la posición
+		// del valor a buscar en el Array.
+		int indiceBusqueda = 0;
+
+		// Algoritmo deb búsqueda secuencial.
+		while (indiceBusqueda < t.length && clave != t[indiceBusqueda]) {
+			indiceBusqueda++;
 		}
-		
-		return result;
+
+		/* Si encuentras un valor mayor o igual que la longitud
+		 * significa que no lo ha encontrado. Por lo que,
+		 * asignaremos -1 para luego retornar dicho -1
+		 * como lo especificado en el ejercicio. */
+		if (indiceBusqueda >= t.length) {
+			indiceBusqueda = -1;
+		}
+
+		// Devolvemos la posición del valor.
+		return indiceBusqueda;
 	}
-	
+
 }
